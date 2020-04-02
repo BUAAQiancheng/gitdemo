16ea321314d35e856df3bf035ad84aa552a1ca9b
qiancheng <1258994272@qq.com>
2020-04-02 16:21:18 +0800

    final update

--- fileinfo.py
+++ fileinfo.py
@@ -6,11 +6,11 @@ Created on Tue Apr  9 15:48:37 2019
 """
 
 dic={'0001':'A','0010':'B','0011':'C','0000':'D','0100':'E','1111':'F','0101':'G','1001':'H'}
-cipher=input()
+c=input()
 a_list=[]
 b_list=[]
-for i in range(0,len(cipher),4):
-    code=cipher[i:i+4]
+for i in range(0,len(c),4):
+    code=c[i:i+4]
     a_list.append(code)
 for every in a_list:
     if every in dic:
