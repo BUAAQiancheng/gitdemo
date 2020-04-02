d55be3f0635b990193137e6e8b7bd3cd622ead07
qiancheng2 <2318141428@qq.com>
2020-04-02 11:47:09 +0800

    update by the second developer

--- fileinfo.py
+++ fileinfo.py
@@ -9,3 +9,6 @@ dic={'0001':'A','0010':'B','0011':'C','0000':'D','0100':'E','1111':'F','0101':'G
 cipher=input()
 a_list=[]
 b_list=[]
+for i in range(0,len(cipher),4):
+    code=cipher[i:i+4]
+    a_list.append(code)
